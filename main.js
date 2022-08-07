90b9af06d5470a40d8f1adddd1f518ea4e24c322
agnekauk <agnenemeikaite@gmail.com>
2022-08-07 13:51:51 +0300

    Responsive design

--- main.js
+++ main.js
@@ -10,18 +10,19 @@ function rand(index) {
 let button = document.querySelector("button");
 
 async function getData() {
-    const requestURL = "https://raw.githubusercontent.com./agnekauk/14-person/main/data.json";
+    const requestURL = './data.json';
     const request = new Request(requestURL);
 
     const response = await fetch(request);
     const JSON = await response.json();
     changedHTML(JSON[rand(JSON.length)]);
 }
+
 button.addEventListener("click", function (e) {
-    confetti({
-        particleCount: 300,
-        spread: 180,
-        startVelocity: 60
-    });
+    // confetti({
+    //     particleCount: 300,
+    //     spread: 180,
+    //     startVelocity: 60
+    // });
     getData();
 })
